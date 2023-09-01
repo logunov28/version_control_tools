a91b33d435dca0a42fbda6ceb0c38588e25e5913
logunov28 <79602109+logunov28@users.noreply.github.com>
2023-09-01 15:35:54 +0300

    Update GIT_file_history.py
    Bug fixes

--- GIT_file_history.py
+++ GIT_file_history.py
@@ -1,5 +1,4 @@
 # Программа показывает историю изменений файла в git
-# Достаточно дарг-эн-дропом перенести файл на иконку программы
 
 import sys, os, subprocess
 
@@ -14,22 +13,22 @@ except:
 filename = droppedFile.split('\\')[-1]
 path = '\\'.join(droppedFile.split('\\')[:-1])
 os.chdir(path)
-out = subprocess.Popen('git blame --incremental ' + filename, stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
-out = out.split('filename')[:-1]
+out = subprocess.Popen('git log ' + filename, stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
+out = out.split('commit')[1:]
 out.reverse()
 output = 'История изменений файла:'
 
+
+counter = 0
 for i in range(len(out)):
 	st = out[i].split('\n')
-	author = 'Unknown'
-	commit = 'Unknown'
-	commit_date = 'Unknown'
-	for j in range(len(st)):
-		if 'author ' in st[j]:
-			author = ' '.join(st[j].split(' ')[1:])
-			commit = st[j-1].split(' ')[0]
-			commit_date = os.popen('git show -s --format=%ci ' + commit).read()
-	output += '\n' + str(i+1) + ') ' + author + ' ' + commit_date[:-7] + ' ' + commit
+	author = st[1][8:]
+	commit = st[0]
+	commit_date = st[2][8:]
+	summary = st[4][4:]
+	if author != 'Unknown':
+		counter +=1
+		output += '\n' + str(i+1) + ') ' + author + ' | ' + commit_date[:-6] + ' |' + commit + '\n   ' + summary
 
 
 print(output)
